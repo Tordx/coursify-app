74f65979438da62f2f51a9afd2369431ee3b7d88
Roven Kyle Narra <ellamateogang@gmail.com>
2023-06-23 17:11:53 +0800

    bottom tabs added for routing

--- src/Routes/bottomtabs.tsx
+++ src/Routes/bottomtabs.tsx
@@ -1,17 +1,82 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { Tabs } from '../../App'
 import Home from '../Screens/Home/Home'
+import Assessment from '../Screens/Home/Assessment'
+import Results from '../Screens/Home/Results'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import { black, errors, mode, theme, white } from '../Assets/Colors'
+import { styles } from '../Assets/Styles'
 
 type Props = {}
+
+const name = 'Juan'
 
 const Bottomtabs = (props: Props) => {
   return (
    <>
-   <Tabs.Navigator>
+   <View style = {{height: 75, width: '100%', backgroundColor: mode ? theme.dark : theme.light, justifyContent: 'center', alignItems: 'center',}}>
+    <View style = {{width: '95%', justifyContent: 'flex-start', alignItems: 'flex-start', flexDirection: 'row'}}>
+      <Text style = {styles.headername}>Hello {name}!</Text>
+      <View style = {styles.headerprofilecontainer}>
+      <Image source={require('../Assets/Images/12.jpg')} style = {styles.headerprofile}/>
+      </View>
+    </View>
+   </View>
+   <Tabs.Navigator
+
+   screenOptions={{
+    headerShown: false,
+    tabBarShowLabel: false,
+    tabBarStyle: { backgroundColor:  mode ? theme.dark : theme.light, borderColor: theme.accenta, height: 70 },
+    tabBarIconStyle: { height: 25, justifyContent: 'center', alignItems: 'center',},
+   }}
+   >
         <Tabs.Screen
             name = 'Home'
             component={Home}
+            options={{
+              
+              tabBarIcon: ({focused}) => (
+                <View style = {styles.tabicon}>
+                  <Icon
+                    name = {focused ? 'book-open-page-variant' : 'book-open-page-variant-outline'}
+                    color = {focused ? mode ? errors.main : theme.accenta : mode ? white.W003 : black.B005}
+                    size = {focused ? 35 : 30}
+                  />
+                </View>
+              )
+            }}
+        />
+        <Tabs.Screen
+            name = 'Assessment'
+            component={Assessment}
+            options={{
+              tabBarIcon: ({focused, color, size}) => (
+                <View style = {styles.tabicon}>
+                  <Icon
+                    name = {focused ? 'notebook-edit' : 'notebook-edit-outline'}
+                    color = {focused ? mode ? errors.main : theme.accenta : mode ? white.W003 : black.B005}
+                    size = {focused ? 35 : 30}
+                  />
+                </View>
+              )
+            }}
+        />
+        <Tabs.Screen
+            name = 'Result'
+            component={Results}
+            options={{
+              tabBarIcon: ({focused, color, size}) => (
+                <View style = {styles.tabicon}>
+                  <Icon
+                    name = {focused ? 'folder-information' : 'folder-information-outline'}
+                    color = {focused ? mode ? errors.main : theme.accenta : mode ? white.W003 : black.B005}
+                    size = {focused ? 35 : 30}
+                  />
+                </View>
+              )
+            }}
         />
    </Tabs.Navigator>
    </>
