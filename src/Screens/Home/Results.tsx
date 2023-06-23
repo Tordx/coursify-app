23a452662b2e61c0b54d2dadefb6f456839345b4
Roven Kyle Narra <ellamateogang@gmail.com>
2023-06-23 17:12:18 +0800

    Results added

---
+++ src/Screens/Home/Results.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { styles } from '../../Assets/Styles'
+
+type Props = {}
+
+const Results = (props: Props) => {
+  return (
+    <View style = {styles.container}>
+      <Text>Results</Text>
+    </View>
+  )
+}
+
+export default Results
