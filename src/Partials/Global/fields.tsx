1028710dd9a3f9a71d7af2519c86e9378c11f860
Roven Kyle Narra <ellamateogang@gmail.com>
2023-06-23 17:11:41 +0800

    added dropdown field

--- src/Partials/Global/fields.tsx
+++ src/Partials/Global/fields.tsx
@@ -1,9 +1,10 @@
-import { View, Text, TextInput, Pressable } from 'react-native'
+import { View, Text, TextInput, Pressable, ViewStyle, TextStyle } from 'react-native'
 import React from 'react'
 import { styles } from '../../Assets/Styles'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-import { mode, theme } from '../../Assets/Colors'
+import { black, mode, theme, transparent, white } from '../../Assets/Colors'
+import SelectDropdown from 'react-native-select-dropdown'
 
 type Props = {
 
@@ -17,7 +18,21 @@ type Props = {
     onPress?: (value: any) => void,
     autoComplete?: string,
 
+
 }
+type DropProps = {
+  
+  data: any,
+  onSelect: (selectedItem: any, index: number) => void,
+  buttonTextAfterSelection: (selectedItem: any, index: number) => string,
+  rowTextForSelection: (item: any, index: number) => string,
+  buttonStyle?: ViewStyle,
+  buttonTextStyle?: TextStyle,
+  name: string,
+  defaultButtonText?: string,
+}
+
+
 
 export const Input = (props: Props) => {
   return (
@@ -53,4 +68,35 @@ export const Input = (props: Props) => {
       </Pressable>
     </View>
   )
+}
+
+export const Dropdown = (props: DropProps) => {
+
+
+
+  return (
+    <View>
+    <SelectDropdown
+      data={props.data}
+      defaultButtonText = {props.defaultButtonText}
+      onSelect={props.onSelect}
+      buttonTextAfterSelection={props.buttonTextAfterSelection}
+      rowTextForSelection={props.rowTextForSelection}
+      buttonStyle = {[styles.logininputcontainer, {backgroundColor: '#00000000', justifyContent: 'center', height: 35}]}
+      buttonTextStyle = {[styles.logininput, {alignSelf: 'flex-start', textAlign: 'left',color: mode ? white.W001 : black.B005, fontSize: 18}]}
+      renderDropdownIcon = {() => (
+        <View style = {{justifyContent: 'center', alignItems: 'center',}}>
+            <Icon
+            name = {props.name || 'blank'}
+            size = {25}
+            color = {mode ? theme.accenta : theme.accentb}
+            style = {{top: -5, left: -5}}
+          />
+          </View>
+      )}
+
+    />
+    </View>
+  )
+
 }
