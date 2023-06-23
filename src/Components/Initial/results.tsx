87686d70fbafd552d1feb809f00ab7d381dcd5aa
Roven Kyle Narra <ellamateogang@gmail.com>
2023-06-23 17:11:25 +0800

    disabled

--- src/Components/Initial/results.tsx
+++ src/Components/Initial/results.tsx
@@ -1,32 +1,32 @@
-import { View, Text } from 'react-native'
-import React, { useState } from 'react'
-import Numpad from './numpad'
-import { useSelector } from 'react-redux'
-import {mode, theme} from '../../Assets/Colors'
+// import { View, Text } from 'react-native'
+// import React, { useState } from 'react'
+// import Numpad from './numpad'
+// import { useSelector } from 'react-redux'
+// import {mode, theme} from '../../Assets/Colors'
 
-type Props = {
+// type Props = {
 
 
-}
+// }
 
-interface data {
+// interface data {
 
-    calcu: {
-        result : string
-    }
-}
+//     calcu: {
+//         result : string
+//     }
+// }
 
-const Results = (props: Props) => {
+// const Results = (props: Props) => {
 
-    const {result} = useSelector((action: data) => action.calcu)
+//     const {result} = useSelector((action: data) => action.calcu)
 
     
 
-  return (
-    <View style = {{ position: 'absolute', top: 100, right: 10}}>
-      <Text style = {{fontSize: 75, color: mode ? theme.light : theme.dark }}>{result}</Text>
-    </View>
-  )
-}
+//   return (
+//     <View style = {{ position: 'absolute', top: 100, right: 10}}>
+//       <Text style = {{fontSize: 75, color: mode ? theme.light : theme.dark }}>{result}</Text>
+//     </View>
+//   )
+// }
 
-export default Results
+// export default Results
