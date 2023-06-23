cef4e96e524335a6b98040439bfbeee4c288cc66
Roven Kyle Narra <ellamateogang@gmail.com>
2023-06-23 17:10:37 +0800

    login title

--- src/Assets/Constants/index.tsx
+++ src/Assets/Constants/index.tsx
@@ -1,3 +1,4 @@
 export const calcuplus = 'calcu++'
-export const appname = 'TruCredit'
+export const appname = 'SIGN IN'
 export const disclosure = 'Access to account creation is exclusive to TruCredit members. If you are interested in applying for a loan, please kindly visit our office during the hours of 8:00AM to 4:00PM, Monday through Friday.'
+export const strand = ["N/A","ABM", "STEM", "CCS", "HUMSS", "GAS", "TVL"]
