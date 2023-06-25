aed178f36a6b6a57f8575bbed7f8e78062e24d1f
Roven Kyle Narra <ellamateogang@gmail.com>
2023-06-25 23:27:38 +0800

    courselist removed log added ui

--- src/Screens/contents/Courselist.tsx
+++ src/Screens/contents/Courselist.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, Image, Pressable } from 'react-native';
 import { styles } from '../../Assets/Styles';
 import { Data } from '../../Library/Firebase';
 import { TouchableOpacity } from 'react-native-gesture-handler';
@@ -8,11 +8,14 @@ import { useSelector } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 import { useDispatch } from 'react-redux';
 import { setschooltitle } from '../../Library/Redux-actions/courseslice';
+import { imageset, school } from '../../Assets/Constants';
+import { errors, theme, white } from '../../Assets/Colors';
+import { TopExit } from '../../Partials/Global/buttons';
 
 
 const Courselist = () => {
 
-
+  
     const course = useSelector((action: Data) => action._course.course);
     const schooltitle = useSelector((action: Data) => action._course.schooltitle);
 
@@ -21,35 +24,48 @@ const Courselist = () => {
     const navigation = useNavigation()
     
   const handleItemClick = async (item: Data) => {
-    // Execute your desired function or code when an item is clicked
-    console.log('====================================item');
-    console.log(item);
+
     dispatch(setschooltitle(item))
-    console.log('====================================item');
     navigation.navigate('CourseOverview' as never)
 
   };
 
-const renderItem = ({ item }: { item: Data }) => (
+  // const renderItem = ({ item }: { item: Data }) =>  {
+    
+  //   const imagesource = imageset[item.course]
+  //     return (
+ 
+  //   )};
+    
+
+const renderItem = ({ item }: { item: Data }) => {
+
+  const imagesource = imageset[item.course]
+  return(
+  
     <View>
-      <TouchableOpacity onPress={() => handleItemClick(item)}>
-        <Text style={{ fontSize: 18, fontWeight: 'bold' , textAlign: 'center' , marginTop: 10, color: 'white' }}>{item.course}</Text>
-        <Text style={{ fontSize: 16 , textAlign: 'center' , color: 'white' }}>{item.description}</Text>
-      </TouchableOpacity>
+          <Pressable onPress={() => handleItemClick(item)}  style = {styles.homecontent}>
+          <Image source={imagesource} style = {styles.imagecontainer} resizeMode='cover'/>
+          <Text style={{ textShadowRadius: 5, textShadowColor: theme.accentd, position: 'absolute', fontSize: 20, fontFamily: 'monthe' , textAlign: 'center' , paddingHorizontal: 30 , color: white.main }}>{item.course}</Text>
+        </Pressable>
 
     </View>
-  );
+  );}
   
   return (
     <View style={styles.container}>
-        <Text style={{ fontSize: 25, fontWeight: 'bold' , textAlign: 'center' , margin: 10, color: 'white' }}>Definition</Text>
-        <Text style={{ fontSize: 20, fontWeight: 'bold' , textAlign: 'center' , margin: 10, color: 'white' }}>{schooltitle.school}</Text>
-      <FlatList
-        data={course}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.course}
+    <Text style={{ fontSize: 50, fontFamily: 'monthe', textAlign: 'center' , margin: 10 , color: errors.W001 }}>SCHOOLS</Text>
+    <Text style={{ fontSize: 20, fontFamily: 'monthe', textAlign: 'center' , margin: 10 , color: 'white' }}>{schooltitle.school}</Text>
+    <FlatList
+    style = {{width: '100%', height: '100%'}}
+      data={course}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.school}
+    />
+    <TopExit
+        onPress={() => navigation.goBack()}
       />
-    </View>
+  </View>
   );
 };
 
