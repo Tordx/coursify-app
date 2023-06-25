bdfaa8d95b6085ac75e0089037e1983ca98105c2
Roven Kyle Narra <ellamateogang@gmail.com>
2023-06-25 20:15:12 +0800

    passing arguments

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, ImageBackground, Pressable, Image } from 'react-native';
 import { styles } from '../../Assets/Styles';
 import { getAllData , Data } from '../../Library/Firebase';
 import { useEffect } from 'react';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { getAllCourse } from '../../Library/Firebase';
+import { getSpecificData } from '../../Library/Firebase';
 import { useNavigation } from '@react-navigation/native';
 import { useDispatch } from 'react-redux';
 import { setcourse } from '../../Library/Redux-actions/courseslice';
 import { setschooltitle } from '../../Library/Redux-actions/courseslice';
-
+import { imageset } from '../../Assets/Constants';
+import { theme, white } from '../../Assets/Colors';
 const Home = () => {
 
   const [data, setData] = useState<Data[]>([]);
   const navigation = useNavigation()
   const dispatch = useDispatch();
 
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const retrievedData: Data[] = await getAllData();
+        const retrievedData: Data[] = await getAllData('course');
         setData(retrievedData);
       } catch (error) {
         console.log('Error fetching data:', error);
@@ -34,7 +34,7 @@ const Home = () => {
   const handleItemClick = async (item: Data) => {
     // Execute your desired function or code when an item is clicked
     try {
-      const data: Data[] = await getAllCourse(item.school);
+      const data: Data[] = await getSpecificData('realcourse', 'school', item.school);
       dispatch(setcourse(data))
       dispatch(setschooltitle(item))
       navigation.navigate('Courselist' as never)
@@ -46,18 +46,21 @@ const Home = () => {
 
   };
 
-  const renderItem = ({ item }: { item: Data }) => (
-    <View>
-       <TouchableOpacity onPress={() => handleItemClick(item)}>
-      <Text style={{ fontSize: 20, fontWeight: 'bold' , textAlign: 'center' , margin: 10 , color: 'white' }}>{item.school}</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  const renderItem = ({ item }: { item: Data }) => {
+    
+  const imagesource = imageset[item.school]
+    return (
+       <Pressable onPress={() => handleItemClick(item)}  style = {styles.homecontent}>
+        <Image source={imagesource} style = {styles.imagecontainer} resizeMode='cover'/>
+        <Text style={{ textShadowRadius: 5, textShadowColor: theme.accentd, position: 'absolute', fontSize: 20, fontFamily: 'monthe' , textAlign: 'center' , padding: 10 , color: white.main }}>{item.school}</Text>
+      </Pressable>
+  )};
 
   return (
     <View style={styles.container}>
-        <Text style={{ fontSize: 30, fontWeight: 'bold' , textAlign: 'center' , margin: 10 , color: 'white' }}>Courses</Text>
+      <Text style={{ fontSize: 30, fontWeight: 'bold' , textAlign: 'center' , margin: 10 , color: 'white' }}>SCHOOLS</Text>
       <FlatList
+      style = {{width: '100%', height: '100%'}}
         data={data}
         renderItem={renderItem}
         keyExtractor={(item) => item.school}
