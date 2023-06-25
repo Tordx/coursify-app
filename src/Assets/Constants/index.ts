799c2665c91b629e4c08be41f3e4c7a561e81e58
Roven Kyle Narra <ellamateogang@gmail.com>
2023-06-25 20:11:54 +0800

    remove old scalequestions

--- src/Assets/Constants/index.ts
+++ src/Assets/Constants/index.ts
@@ -2,25 +2,8 @@ export const calcuplus = 'calcu++'
 export const appname = 'SIGN IN'
 export const disclosure = 'Access to account creation is exclusive to TruCredit members. If you are interested in applying for a loan, please kindly visit our office during the hours of 8:00AM to 4:00PM, Monday through Friday.'
 export const genders = ["Male", "Female", "Others"]
-export const strand = ["N/A","ABM", "STEM", "CCS", "HUMSS", "GAS", "TVL"]
-// export const scaleQuestions: string[] = [
-//     "How interested are you in mathematics?",
-//     "How much do you enjoy problem-solving?",
-//     "How creative do you consider yourself to be?",
-//     "How much do you enjoy working with computers?",
-//     "How interested are you in scientific subjects?",
-//     "How much do you enjoy reading and analyzing literature?",
-//     "How comfortable are you with public speaking?",
-//     "How interested are you in learning about different cultures and languages?",
-//     "How much do you enjoy working with your hands or creating physical objects?",
-//     "How interested are you in studying business and entrepreneurship?",
-//     "How much do you enjoy exploring and understanding historical events?",
-//     "How interested are you in environmental and sustainability issues?",
-//     "How much do you enjoy analyzing data and solving analytical problems?",
-//     "How interested are you in the field of healthcare and helping others?",
-//     "How much do you enjoy working in a team and collaborating with others?",
-//   ];
 
+export const strand = ["N/A","ABM", "STEM", "CCS", "HUMSS", "GAS", "TVL"]
   export const school = [
     {
       Enginerring: [
@@ -600,3 +583,11 @@ export const strand = ["N/A","ABM", "STEM", "CCS", "HUMSS", "GAS", "TVL"]
       "5": "BACHELOR OF SCIENCE IN CRIMINOLOGY",
     },
   ]
+
+  export const imageset: {[key: string]: any} = {
+
+    "SCHOOL OF INFORMATION TECHNOLOGY": require('../Images/BSIT.jpg'),
+    "SCHOOL OF ENGINEERING": require('../Images/CIVENG.jpg'),
+    "SCHOOL OF TEACHER EDUCATION": require('../Images/TEACHINGS.jpg')
+
+  }
