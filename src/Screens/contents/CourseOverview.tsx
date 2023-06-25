dd24e3160c0afd4e34f3584c9bbc15ed87cca0bb
Roven Kyle Narra <ellamateogang@gmail.com>
2023-06-25 23:52:17 +0800

    styling added

--- src/Screens/contents/CourseOverview.tsx
+++ src/Screens/contents/CourseOverview.tsx
@@ -1,22 +1,27 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { styles } from '../../Assets/Styles';
 import { Data } from '../../Library/Firebase';
 import { useSelector } from 'react-redux';
 import { TopExit } from '../../Partials/Global/buttons';
 import { useNavigation } from '@react-navigation/native';
+import { imageset } from '../../Assets/Constants';
+import { errors, theme, white } from '../../Assets/Colors';
 
 const CourseOverView = () => {
 
     const schooltitle = useSelector((action: Data) => action._course.schooltitle);
     const navigation = useNavigation()
-    
+    const imagesource = imageset[schooltitle.course]
   return (
     <View style={styles.container}>
-        <Text style={{ fontSize: 25, fontWeight: 'bold' , textAlign: 'center' , margin: 13 , color: 'white' }}>Course Overview</Text>
-        <Text style={{ fontSize: 20, fontWeight: 'bold' , textAlign: 'center' , margin: 8 , color: 'white' }}>{schooltitle.school}</Text>
-        <Text style={{ fontSize: 20, fontWeight: 'bold' , textAlign: 'center' , margin: 6 , color: 'white' }}>{schooltitle.course}</Text>
-        <Text style={{ fontSize: 20, fontWeight: 'normal' , textAlign: 'center' , margin: 5 , color: 'white' }}>{schooltitle.description}</Text>
+        <View style = {{position: 'absolute', top: 0, width: '100%'}}>
+        <Image source = {imagesource} style = {{ width: '100%', height: 300}} resizeMode='cover' />
+        <Text style={{ fontSize: 50, fontFamily: 'monthe' , textAlign: 'center' , marginTop: 8 , color: errors.main, textDecorationLine: 'underline'}}>OVERVIEW</Text>
+        <Text style={{ fontSize: 20, fontFamily: 'monthe' , textAlign: 'center' , marginBottom: 8 , color: 'white' }}>{schooltitle.school}</Text>
+        <Text style={{ fontSize: 50, fontFamily: 'monthe' , textAlign: 'left' , margin: 6 , color: 'white', paddingLeft: 10 }}>{schooltitle.course}</Text>
+        <Text style={{ fontSize: 30, fontFamily: 'montel' , textAlign: 'left' , margin: 5 , color: 'white' }}>{schooltitle.description}</Text>
+        </View>
         <TopExit
         onPress={() => navigation.goBack()}
       />
