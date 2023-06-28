82aa826c8a3cb160aa9bcefd5e1e47da0be29b1f
Roven Kyle Narra <ellamateogang@gmail.com>
2023-06-29 01:51:30 +0800

    added reset password

--- src/Screens/Initial/reset.tsx
+++ src/Screens/Initial/reset.tsx
@@ -7,12 +7,40 @@ import { LoginButton, TopExit } from '../../Partials/Global/buttons'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import { useNavigation } from '@react-navigation/native'
 import { openEmailApp } from '../../Library/Functions'
+import { firebase } from '@react-native-firebase/auth'
+import { AlertModal, SuccessModal } from '../../Partials/Global/modals'
 type Props = {}
 
 const Reset = (props: Props) => {
 
-    const [email, setemail] = useState('')
-    const navigation = useNavigation()
+    const [email, setemail] = useState('');
+    const [success, setsuccess] = useState(false)
+    const [alertmodal, setalertmodal] = useState(false);
+    const [title, settitle] = useState('')
+    const navigation = useNavigation();
+
+    const resetpassword = async() => {
+      try {
+      if (email) {
+      await firebase.auth().sendPasswordResetEmail(email).then((response: any) => {
+        console.log('sent password')
+        settitle('Password reset Link sent')
+        setsuccess(true)
+        
+
+    
+      })
+      } else {
+        setalertmodal(true);
+        settitle('Email is empty')
+      }
+      } catch (error: any){
+        if (error.code === 'auth/user-not-found') {
+          setalertmodal(true);
+          settitle('Email not found')
+        }
+      }
+    }
 
   return (
     <View style = {styles.container}>
@@ -32,6 +60,7 @@ const Reset = (props: Props) => {
         />
         <LoginButton
             title='Reset Password'
+            onPress={resetpassword}
         />
         <Pressable 
             onPress={openEmailApp} 
@@ -40,6 +69,18 @@ const Reset = (props: Props) => {
           >
           <Text style  = {[styles.vermodaltext, {fontSize: 14}]}>Having trouble with your account? Contact Us</Text>
             </Pressable>
+        <AlertModal
+          title={title}
+          visible = {alertmodal}
+          onRequestClose={() => setalertmodal(false)}
+          onPress={() => setalertmodal(false)}
+        />
+        <SuccessModal
+          visible = {success}
+          title={title}
+          onPress={() => setsuccess(false)}
+          onRequestClose={() => setsuccess(false)}
+        />
         <TopExit
             onPress={() => {navigation.goBack()}}
         />
